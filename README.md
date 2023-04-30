6ed02c5294eb50ca17e8d814c31f76bee89ee9c8
shubham <shrivastavashubham596@gmail.com>
2023-04-30 20:13:25 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# -Build-a-COMPLETE-Fullstack-ecommerce-Responsive-MERN-App-React-Redux-Nodejs-MongoDB-Express
+ Build a COMPLETE Fullstack ecommerce Responsive MERN App | React, Redux, Nodejs, MongoDB, Express
+
+Backend Environment variables  : 
+
+MONGODB_URL : ""
+
+start : 
+
+## npm run dev 
+
+
+Frontend Environment variables  :
+
+REACT_APP_SERVER_DOMIN = http://localhost:8080
+
+REACT_APP_ADMIN_EMAIL = <admin email id >
+
+start : 
+
+## npm start
---
+++ frontend/src/page/Contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div>
+    <p>Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi.</p>
+    </div>
+  )
+}
+
+export default Contact
